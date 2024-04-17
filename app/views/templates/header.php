c0bfe43e75355c664c35d9728b84ceff4575c881
Rennnn12 <rennhaff@gmail.com>
2024-04-16 19:24:12 -0700

    first init github

--- app/views/templates/header.php
+++ app/views/templates/header.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>PERPUS RPL | <?= getTitle(); ?></title>
+  <title>PERPUSTAKAAN RPL | <?= getTitle(); ?></title>
 
   <!-- Google Font: Source Sans Pro -->
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
@@ -31,7 +31,7 @@
   <!-- /.navbar -->
 
   <!-- Main Sidebar Container -->
-  <aside class="main-sidebar sidebar-gray-primary elevation-4">
+  <aside class="main-sidebar sidebar-dark-primary elevation-4">
 
     <!-- Sidebar -->
     <div class="sidebar">
